d5a4793b9587a9ed2c0be3bc813b8e0f70e0d1e0
flcat <flcat.xd@gmail.com>
2024-01-26 21:58:07 +0900

    initttt

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM openjdk:17
-# ARG로 프로필 변수 정의
-ARG PROFILES
-# 이건 나중에 docker-compose에서 초기화 해줍니다.
-ARG ENV
-ARG JAR_FILE=build/libs/*.jar
-WORKDIR /root
-# JAR 파일 메인 디렉토리에 복사
-COPY ./*.jar .
 
-# 시스템 진입점 정의
-# ENTRYPOINT ["java", "-jar", "/app.jar"]
-CMD java -jar test-0.0.1-SNAPSHOT.jar
+WORKDIR /usr/src/app
+
+ARG JAR_PATH=./build/libs
+
+COPY ${JAR_PATH}/test-0.0.1-SNAPSHOT.jar ${JAR_PATH}/test-0.0.1-SNAPSHOT.jar
+
+CMD ["java","-jar","./build/libs/test-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -1,20 +1,11 @@
 package com.example.test;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
 public class TestApplication {
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
-			+ "classpath:real-application.yml";
-
 	public static void main(String[] args) {
-
-		new SpringApplicationBuilder(TestApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(TestApplication.class, args);
 	}
 }
