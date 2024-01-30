3cc410085338d21c9471d7e1c4b89005f54e6f62
flcat <flcat.xd@gmail.com>
2024-01-30 21:05:37 +0900

    ci/cd test

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM openjdk:17
 
-WORKDIR /usr/src/app
+WORKDIR /root
 
 ARG JAR_PATH=./build/libs
 
 COPY ${JAR_PATH}/test-0.0.1-SNAPSHOT.jar ${JAR_PATH}/test-0.0.1-SNAPSHOT.jar
 
-CMD ["java","-jar","./build/libs/test-0.0.1-SNAPSHOT.jar"]
+#CMD ["java","-jar","./build/libs/test-0.0.1-SNAPSHOT.jar"]
+CMD java -jar -Dspring.profiles.active=${active} test-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/test/controller/DockerTestController.java
+++ src/main/java/com/example/test/controller/DockerTestController.java
@@ -1,13 +1,34 @@
 package com.example.test.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class DockerTestController {
+
+    private final Environment env;
 
     @GetMapping("/")
     public String home() {
-        return "Hello world";
+
+        List<String> profile = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
+
+        return profile.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "무중단 배포 !!";
     }
 }
